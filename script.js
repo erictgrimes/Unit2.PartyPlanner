478e701f0d99693f5620ab34e84986257b382072
Eric Grimes <erictgrimes@gmail.com>
2025-05-31 11:45:33 -0700

    HTML fully setup

---
+++ script.js
@@ -0,0 +1,15 @@
+// A list of parties from and api with name, date, time, location, and description are populated on the page when loaded
+// Each party has a delete button that removes the party from the list and the api
+// There is also a form where the user can add a new party
+// When the add button is clicked the party is added to the page and the api
+
+//My class code
+const COHORT = "2503-FTB-ET-WEB-AM";
+
+//Api Url
+const API_URL = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/${COHORT}/events`;
+
+
+
+
+
